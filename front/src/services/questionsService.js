92737794d95dcc8ca8deed8efbcdb67d418bbbd9
LiorKorssia22 <korssia2@gmail.com>
2023-01-19 14:31:11 +0200

    add manage question

---
+++ front/src/services/questionsService.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+export function QuestionService() {
+
+   const getQuestion = async () => {
+        try {
+          const response = await axios.get('http://localhost:3001/questions');
+          return response.data;
+        } catch (error) {
+          console.error(error);
+        }
+     }
+
+     const filterBySubject = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/questions/filter');
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+     };
+
+     const addQuestion = async () => {
+      try {
+        const response = await axios.post('http://localhost:3001/questions');
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+     };
+
+     const deleteQuestion = async (_id) => {
+       try {
+        const response = await axios.delete('http://localhost:3001/questions/'+_id);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+     };
+
+     const editQuestion = async (_id) => {
+      try {
+        const response = await axios.put('http://localhost:3001/questions/'+_id);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+     };
+
+
+    return {
+        getQuestion,
+
+        addQuestion,
+        deleteQuestion,
+        editQuestion
+     };
+}
+
+
