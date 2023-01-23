8f6edbf538ed4310fdad1815745482a8ebdde093
Ariel Joseph <arieljoseph111@gmail.com>
2023-01-24 01:38:01 +0200

    Fixed known conflicts client also, ya ya am a junior

--- front/src/components/Question/questionsPage.js
+++ front/src/components/Question/questionsPage.js
@@ -1,17 +1,17 @@
-import React, { useEffect , useState} from "react";
-import { useNavigate, useParams  } from "react-router-dom";
-import {QuestionService} from "../../services/questionsService";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { QuestionService } from "../../services/questionsService";
 import "./questionsPage.css";
 
 export function QuestionsPage() {
-  const  [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState([]);
 
   const service = QuestionService();
 
   let navigate = useNavigate();
 
   async function deleteData(_id) {
-    if (window.confirm('Are you sure?')){
+    if (window.confirm("Are you sure?")) {
       const arr = await service.deleteQuestion(_id);
       setQuestions(arr);
     }
@@ -32,71 +32,84 @@ export function QuestionsPage() {
   //   navigate('/Players', {
   //     userId: id,
   //   });
-   
+
   // };
 
-
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchData() {
       const arr = await service.getQuestions();
       setQuestions(arr);
     }
     fetchData();
-  },[])
-
+  }, []);
 
   return (
     <div>
       <div className="topnav">
-        <button className="btnNav" onClick={() => navigate("../")}>Home</button>
+        <button className="btnNav" onClick={() => navigate("../")}>
+          Home
+        </button>
       </div>
       <div>
-      <form className="d-flex">
-        <input className="form-control me-2" type="text" placeholder="Search..." ></input>
-        <button className="btn btn-primary" type="button">Search</button>
-      </form>
-
-
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="text"
+            placeholder="Search..."
+          ></input>
+          <button className="btn btn-primary" type="button">
+            Search
+          </button>
+        </form>
       </div>
 
       <h1>Questions</h1>
       <div>
-        <button onClick={() => navigate("/newQuestionPage")}>New Question</button>
+        <button onClick={() => navigate("/newQuestionPage")}>
+          New Question
+        </button>
       </div>
-    
+
       <table className="table table-striped">
         <tbody>
-
-       
-        <tr>
-          <th>Id</th>
-          <th>question text</th>
-          <th>last update</th>
-          <th>question type</th>
-          <th>function</th>
-        </tr>
-        {questions?.map((item, key) => {
-          // _id, number, subject, body, answers, correctAnswer, quizzes
-          return (
-            <tr key={key}>
-              <td>{item._id}</td>
-              <td>{item.body}</td>
-              <td>{item.date}</td>
-              <td>{item.type}</td>
-              <td>
-                <button className="btn btn-success" onClick={() => showData(item._id)}>show</button> 
-                <button  className="btn btn-success" onClick={() => navigate(`/editQuestionPage/${item._id}`)}>edit</button> 
-                <button onClick={() => deleteData(item._id)} className="btn btn-danger">delete</button>
-              </td>
-            </tr>
-          )
-        })}
-         </tbody>
+          <tr>
+            <th>Id</th>
+            <th>question text</th>
+            <th>question type</th>
+            <th>function</th>
+          </tr>
+          {questions?.map((item, key) => {
+            // _id, number, subject, body, answers, correctAnswer, quizzes
+            return (
+              <tr key={key}>
+                <td>{item._id}</td>
+                <td>{item.body}</td>
+                <td>{item.type}</td>
+                <td>
+                  <button
+                    className="btn btn-success"
+                    onClick={() => showData(item._id)}
+                  >
+                    show
+                  </button>
+                  <button
+                    className="btn btn-success"
+                    onClick={() => navigate(`/editQuestionPage/${item._id}`)}
+                  >
+                    edit
+                  </button>
+                  <button
+                    onClick={() => deleteData(item._id)}
+                    className="btn btn-danger"
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
-
-     
     </div>
   );
 }
-
-
--- front/src/components/quizzes/quizzesPage.js
+++ front/src/components/quizzes/quizzesPage.js
@@ -1,34 +1,33 @@
 import "./quizzesPage.css";
-import React, { useEffect , useState} from "react";
-import { useNavigate, useParams  } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 // import { QuizzesService } from "../../services/quizzesService";
 import { QuizzesService } from "../../services/quizzesService";
 
 export function QuizzesPage() {
-  const  [quizzes, setQuizzes] = useState([]);
+  const [quizzes, setQuizzes] = useState([]);
 
   const service = QuizzesService();
 
   let navigate = useNavigate();
 
-
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchData() {
       const arr = await service.getQuizzes();
       setQuizzes(arr);
       console.log(arr);
     }
     fetchData();
-  },[])
+  }, []);
 
   const linkClickHandler = (id) => {
     let url = `http://localhost:3000/studentQuiz/${id}`;
     navigator.clipboard.writeText(url);
-    alert("link Copy!")
+    alert("link Copy!");
   };
 
   async function deleteData(_id) {
-    if (window.confirm('Are you sure?')){
+    if (window.confirm("Are you sure?")) {
       const arr = await service.deleteQuiz(_id);
       setQuizzes(arr);
     }
@@ -41,61 +40,69 @@ export function QuizzesPage() {
 
   return (
     <div>
-
-
       <div>
-      <div className="topnav">
-        <button className="btnNav" onClick={() => navigate("../")}>Home</button>
-      </div>
-      <div>
-      <form class="d-flex">
-        <input className="form-control me-2" type="text" placeholder="Search..."></input>
-        <button className="btn btn-primary" type="button">Search</button>
-      </form>
+        <div className="topnav">
+          <button className="btnNav" onClick={() => navigate("../")}>
+            Home
+          </button>
+        </div>
+        <div>
+          <form class="d-flex">
+            <input type="text" placeholder="Search" />
+            <button className="btn btn-primary" type="button">
+              Search
+            </button>
+          </form>
 
-      <input type='text'  placeholder='Search' />
-      <h1>QuizzesPage</h1>
-      <div style={{ display: "flex", display: "inline-block" }}>
-        <button onClick={() => navigate("/quizzespage/newquiz")}>Generate New</button>
-      </div>
+          <h1>QuizzesPage</h1>
+          <div style={{ display: "flex", display: "inline-block" }}>
+            <button onClick={() => navigate("/quizzespage/newquiz")}>
+              Generate New
+            </button>
+          </div>
+        </div>
 
-      </div>
-
-     
-    
-      <table className="table table-striped">
-        <tr>
-          <th>Id</th>
-          <th>Link</th>
-          <th>Test Name</th>
-          <th>subject</th>
-          <th>number of question</th>
-          <th>function</th>
-        </tr>
-        {quizzes?.map((item, key) => {
-          // _id, number, subject, body, answers, correctAnswer, quizzes
-          //  name: String,
-  // subject: String,
-  // questions:
-          return (
-            <tr key={key}>
-              <td>{item._id}</td>
-              <td><button onClick={() => linkClickHandler(item._id)} >Link</button></td>
-              <td>{item.name}</td>
-              <td>{item.subject}</td>
-              <td>{item.questions}</td>
-              <td>
-                <button onClick={() => showData(item._id)} className="btn btn-success">Show</button> 
-                <button  className="btn btn-success">Edit</button> 
-                <button onClick={() => deleteData(item._id)} className="btn btn-danger">Delete</button>
-              </td>
+        <table className="table table-striped">
+          <tbody>
+            <tr>
+              <th>Id</th>
+              <th>Link</th>
+              <th>Test Name</th>
+              <th>subject</th>
+              <th>function</th>
             </tr>
-          )
-        })}
-      </table>
-
-     
-    </div>
+            {quizzes?.map((item, key) => {
+              return (
+                <tr key={key}>
+                  <td>{item._id}</td>
+                  <td>
+                    <button onClick={() => linkClickHandler(item._id)}>
+                      Link
+                    </button>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.subject}</td>
+                  <td>
+                    <button
+                      onClick={() => showData(item._id)}
+                      className="btn btn-success"
+                    >
+                      Show
+                    </button>
+                    <button className="btn btn-success">Edit</button>
+                    <button
+                      onClick={() => deleteData(item._id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
